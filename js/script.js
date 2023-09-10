3bc8a999dedd45e276c0fdcb24e1f3072a8940d9
faustinopsy <rodrigohipnose@gmail.com>
2023-09-10 14:53:02 -0300

    modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,80 @@
 async function loadModels() {
   try {
-      await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-      await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+      await faceapi.nets.tinyFaceDetector.loadFromUri('./models');
+      await faceapi.nets.ssdMobilenetv1.loadFromUri("./models");
+      await faceapi.nets.faceLandmark68Net.loadFromUri("./models");
+      await faceapi.nets.faceRecognitionNet.loadFromUri("./models");
   } catch (error) {
       console.error("Erro ao carregar os modelos:", error);
   }
 }
 
-async function detectFacesOnImage(image) {
-  const canvas = document.getElementById('canvas');
-  const context = canvas.getContext('2d');
-  context.drawImage(image, 0, 0, 400, 400); 
+let firstDescriptor = null;
 
-  try {
-      const detections = await faceapi.detectAllFaces(canvas, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
-      
-      if (detections && detections.length > 0) {
-          faceapi.draw.drawDetections(canvas, detections);
-          faceapi.draw.drawFaceLandmarks(canvas, detections);
-      } else {
-          console.log("Nenhuma face detectada.");
-      }
-  } catch (error) {
-      console.error("Erro durante a detecção:", error);
+async function processFirstImage(image) {
+  const canvas = document.getElementById('canvas1');
+  canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height); // Desenha a imagem no canvas
+
+  const detection = await faceapi.detectSingleFace(canvas).withFaceLandmarks().withFaceDescriptor();
+
+  if (detection) {
+      firstDescriptor = detection.descriptor;
+      return true;
   }
+
+  return false;
 }
 
-document.getElementById('imageUpload').addEventListener('change', function() {
-  const image = new Image();
-  image.src = URL.createObjectURL(document.getElementById('imageUpload').files[0]);
-  image.onload = function() {
-      detectFacesOnImage(image);
-  };
+async function processSecondImage(image) {
+  const canvas = document.getElementById('canvas2');
+  const context = canvas.getContext('2d');
+
+  context.drawImage(image, 0, 0, canvas.width, canvas.height); 
+
+  const detections = await faceapi.detectAllFaces(canvas).withFaceLandmarks().withFaceDescriptors();
+
+  detections.forEach(det => {
+    const distance = Math.round(faceapi.euclideanDistance(firstDescriptor, det.descriptor) * 100) / 100; 
+    const similarityPercentage = Math.max(0, (1 - distance / 0.6) * 100);
+    const roundedPercentage = Math.round(similarityPercentage);
+    
+    if (roundedPercentage > 80) {
+      context.lineWidth = 4; 
+        context.strokeStyle = '#00FF00'; 
+        context.fillStyle = '#00FF00';
+    } else {
+      context.lineWidth = 2; 
+        context.strokeStyle = '#FF0000'; 
+        context.fillStyle = '#FF0000';
+    }
+
+    context.strokeRect(det.detection.box.x, det.detection.box.y, det.detection.box.width, det.detection.box.height);
+
+    context.font = '18px Arial'; 
+    context.fillText(`Similaridade: ${roundedPercentage} %`, det.detection.box.x, det.detection.box.y - 10); 
+});
+
+}
+
+
+document.getElementById('imageUpload2').addEventListener('change', async function() {
+  const image1 = new Image();
+  image1.src = URL.createObjectURL(document.getElementById('imageUpload1').files[0]);
+  await new Promise(res => {
+      image1.onload = async function() {
+          if (await processFirstImage(image1)) {
+              const image2 = new Image();
+              image2.src = URL.createObjectURL(document.getElementById('imageUpload2').files[0]);
+              image2.onload = function() {
+                  processSecondImage(image2);
+              };
+          }
+      };
+      image1.onerror = err => {
+          console.error("Erro ao carregar a primeira imagem:", err);
+          res();
+      };
+  });
 });
 
 loadModels();
